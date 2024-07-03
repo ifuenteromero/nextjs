0c572b4e2c182ab6fd94d37e5b029ad2fd5dae72
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-03 18:59:42 +0200

    Fetching data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,20 @@
-const UsersPage = () => {
+interface User {
+	id: number;
+	name: string;
+}
+
+const UsersPage = async () => {
+	const endpoint = 'https://jsonplaceholder.typicode.com/users';
+	const users: User[] = await fetch(endpoint).then((res) => res.json());
 	return (
-		<div>
-			<p>UsersPage</p>
-		</div>
+		<>
+			<h1>User List</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+		</>
 	);
 };
 
