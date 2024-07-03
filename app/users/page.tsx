63a4969163fc045d63a468e80a775d64e4ca4516
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-03 19:50:21 +0200

    Static an dynamic rendering Por defecto fetch cachea las peticiones. Al hacer npm run build si está cacheado la hora que saldrá será en tiempo de construcción y si le pones cache no store para que los datos sean frescos irá cambiando la hora porque es en request time

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,19 +6,25 @@ interface User {
 const UsersPage = async () => {
 	const endpoint = 'https://jsonplaceholder.typicode.com/users';
 
+	// RENDERING => CLIENT SIDE O SERVER SIDE
+	// SERVER SIDE RENDERING CAN BE => STATIC (at build time) or dynamic (at request time)
+
 	const res = await fetch(
-		endpoint,
+		endpoint
 		// {
 		// 	next: {
 		// 		revalidate: 10,
 		// 	},
 		// }
-		{ cache: 'no-store' } // always fresh data
+		// { cache: 'no-store' } // always fresh data
 	);
 	const users: User[] = await res.json();
+
+	const now = new Date().toLocaleTimeString();
 	return (
 		<>
 			<h1>User List</h1>
+			<p>{now}</p>
 			<ul>
 				{users.map((user) => (
 					<li key={user.id}>{user.name}</li>
