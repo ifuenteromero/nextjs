233f9ad3c1987821322fab55bf985a8eacb3645d
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-08 20:26:56 +0200

    tailwind

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,12 @@
 import AddToCart from './AddToCart';
-import styles from './ProductCard.module.css';
+// import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
 	return (
-		<div className={styles.card}>
+		<div
+			// className={styles.card}
+			className='p-4 my-2 text-2xl bg-sky-400 text-cyan-900 hover:bg-sky-950 hover:text-white'
+		>
 			<AddToCart />
 		</div>
 	);
