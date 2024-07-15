cb1715b446717860336d53a57681012fe2e05ae7
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-15 18:16:48 +0200

    Getting a single object

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const GET = (
+	request: NextRequest,
+	{
+		params: { id },
+	}: {
+		params: { id: number };
+	}
+) => {
+	if (id > 10)
+		return NextResponse.json({ error: 'User not found' }, { status: 404 });
+	return NextResponse.json({ id: 1, name: 'Irene' });
+};
