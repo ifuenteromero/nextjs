f8e8143570126d57185dc4f483c24ae0a3574516
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-15 19:27:36 +0200

    Creating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -9,3 +9,13 @@ export const GET = (request: NextRequest) =>
 		{ id: 2, name: 'Elvira' },
 		{ id: 3, name: 'Sara' },
 	]);
+
+export const POST = async (request: NextRequest) => {
+	const body = await request.json();
+	if (!body.name)
+		return NextResponse.json(
+			{ error: 'Name is required' },
+			{ status: 400 }
+		);
+	return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+};
