e57f988552889198e66db399719e618db9c28c1c
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-15 19:49:58 +0200

    Updating an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -12,3 +12,23 @@ export const GET = (
 		return NextResponse.json({ error: 'User not found' }, { status: 404 });
 	return NextResponse.json({ id: 1, name: 'Irene' });
 };
+
+export const PUT = async (
+	request: NextRequest,
+	{
+		params: { id },
+	}: {
+		params: { id: number };
+	}
+) => {
+	const body = await request.json();
+	if (!body.name)
+		return NextResponse.json(
+			{ error: 'Name is required' },
+			{ status: 400 }
+		);
+
+	if (id > 10)
+		return NextResponse.json({ error: 'User not found' }, { status: 404 });
+	return NextResponse.json({ id, name: body.name });
+};
