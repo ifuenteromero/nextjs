4021e3b741b7d580db24546375530983a20ad26a
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-17 16:30:12 +0200

    Updating data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -21,18 +21,41 @@ export const PUT = async (
 	{
 		params: { id },
 	}: {
-		params: { id: number };
+		params: { id: string };
 	}
 ) => {
+	const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
+	if (!user)
+		return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
 	const body = await request.json();
-	const validation = schema.safeParse(body);
+	if (body.email) {
+		const emailAlreadyUsed = await prisma.user.findFirst({
+			where: {
+				AND: [{ email: body.email }, { NOT: { id: parseInt(id) } }],
+			},
+		});
+		if (emailAlreadyUsed)
+			return NextResponse.json(
+				{ error: 'This email is already used' },
+				{ status: 400 }
+			);
+	}
+
+	const savedUser = schema.strip().safeParse(user).data;
+
+	const newUser = { ...savedUser, ...body };
+	const validation = schema.safeParse(newUser);
 
 	if (!validation.success)
 		return NextResponse.json(validation.error.errors, { status: 400 });
 
-	if (id > 10)
-		return NextResponse.json({ error: 'User not found' }, { status: 404 });
-	return NextResponse.json({ id, name: body.name });
+	const updatedUser = await prisma.user.update({
+		where: { id: parseInt(id) },
+		data: newUser,
+	});
+
+	return NextResponse.json(updatedUser);
 };
 
 export const DELETE = (
