1e89c030152600d83a90b1246d40e94c5ab94eb8
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-18 18:43:24 +0200

    Showing uploaded images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,15 +1,39 @@
 'use client';
-import { CldUploadWidget } from 'next-cloudinary';
+import { CldImage, CldUploadWidget } from 'next-cloudinary';
+import { useState } from 'react';
+
+interface CloudinaryResult {
+	public_id: string;
+}
 
 const UploadPage = () => {
+	const [publicId, setPublicId] = useState('');
 	return (
-		<CldUploadWidget uploadPreset='nextapp'>
-			{({ open }) => (
-				<button className='btn btn-primary' onClick={() => open()}>
-					Upload
-				</button>
+		<>
+			{publicId && (
+				<CldImage
+					src={publicId}
+					width={270}
+					height={180}
+					alt='A random image'
+				/>
 			)}
-		</CldUploadWidget>
+			<CldUploadWidget
+				uploadPreset='pox2mbbq'
+				onSuccess={(result, widget) => {
+					console.log({ result, widget });
+					const info = result.info as CloudinaryResult;
+					const _publicId = info.public_id;
+					setPublicId(_publicId);
+				}}
+			>
+				{({ open }) => (
+					<button className='btn btn-primary' onClick={() => open()}>
+						Upload
+					</button>
+				)}
+			</CldUploadWidget>
+		</>
 	);
 };
 
