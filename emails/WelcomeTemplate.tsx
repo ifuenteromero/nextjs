49707306a75fd63c589340a6368802fb9c4c3914
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-31 17:21:14 +0200

    Styling emails with css

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -6,19 +6,28 @@ import {
 	Preview,
 	Text,
 } from '@react-email/components';
+import { CSSProperties } from 'react';
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
 	return (
 		<Html>
 			<Preview>Welcome aboard!</Preview>
-			<Body>
+			<Body style={body}>
 				<Container>
-					<Text>Hello {name}</Text>
+					<Text style={heading}>Hello {name}</Text>
 					<Link href='https://www.google.com'>google.com</Link>
 				</Container>
 			</Body>
 		</Html>
 	);
+};
+
+const body: CSSProperties = {
+	background: '#fff',
+};
+
+const heading: CSSProperties = {
+	fontSize: 32,
 };
 
 export default WelcomeTemplate;
