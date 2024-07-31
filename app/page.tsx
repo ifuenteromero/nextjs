9d2c573e1cf5f8e1ae4a53f5fe26892f2a011527
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-31 18:28:02 +0200

    Optimizing local images

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,12 @@
-import { getServerSession } from 'next-auth';
-import Link from 'next/link';
-import { authOptions } from './api/auth/[...nextauth]/route';
-import ProductCard from './components/ProductCard';
+// a pesar de que  es un archivo .jpg, se visualiza como un archivo .webp
+// el tamaño aparece como memorizado, si desactivamos la cache, se puede ver el tamaño reducido
+import dj from '@/public/images/dj.jpg';
+import Image from 'next/image';
 
 export default async function Home() {
-	const session = await getServerSession(authOptions);
 	return (
 		<main>
-			<h1>Hello {session?.user?.name}</h1>
-			<Link href='/users'>Users</Link>
-			<Link
-				style={{ display: 'block' }}
-				href='/products/grocery/dairy/milk'
-			>
-				/products/grocery/dairy/milk{' '}
-			</Link>
-			<Link style={{ display: 'block' }} href='/products'>
-				/products no funciona
-			</Link>
-			<Link
-				style={{ display: 'block' }}
-				href='/cities/castillaLaMancha/Cuenca/Chillaron'
-			>
-				/cities/castillaLaMancha/Cuenca/Chillaron
-			</Link>
-			<Link style={{ display: 'block' }} href='/cities'>
-				/cities parametro opcional
-			</Link>
-			<ProductCard />
+			<Image src={dj} alt='dj' />
 		</main>
 	);
 }
