b99f18aa5a0b224480503438a82bde3c8ba1a411
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-31 18:32:13 +0200

    Optimizing remote images

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 // a pesar de que  es un archivo .jpg, se visualiza como un archivo .webp
 // el tamaño aparece como memorizado, si desactivamos la cache, se puede ver el tamaño reducido
-import dj from '@/public/images/dj.jpg';
+// import dj from '@/public/images/dj.jpg';
 import Image from 'next/image';
 
 export default async function Home() {
 	return (
 		<main>
-			<Image src={dj} alt='dj' />
+			<Image
+				// src={dj}
+				// alt='dj'
+				src='https://bit.ly/react-cover'
+				alt='react logo'
+				width={300}
+				height={170}
+			/>
 		</main>
 	);
 }
