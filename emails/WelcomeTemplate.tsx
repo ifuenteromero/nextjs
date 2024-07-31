03c3076cf106678ec26a7729d23d686253efc8c2
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-07-31 17:24:34 +0200

    styling emails with tailwind

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -4,30 +4,41 @@ import {
 	Html,
 	Link,
 	Preview,
+	Tailwind,
 	Text,
 } from '@react-email/components';
-import { CSSProperties } from 'react';
+// import { CSSProperties } from 'react';
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
 	return (
 		<Html>
 			<Preview>Welcome aboard!</Preview>
-			<Body style={body}>
-				<Container>
-					<Text style={heading}>Hello {name}</Text>
-					<Link href='https://www.google.com'>google.com</Link>
-				</Container>
-			</Body>
+			<Tailwind>
+				<Body
+					// style={body}
+					className='bg-white'
+				>
+					<Container>
+						<Text
+							// style={heading}
+							className='text-3xl font-bold'
+						>
+							Hello {name}
+						</Text>
+						<Link href='https://www.google.com'>google.com</Link>
+					</Container>
+				</Body>
+			</Tailwind>
 		</Html>
 	);
 };
 
-const body: CSSProperties = {
-	background: '#fff',
-};
+// const body: CSSProperties = {
+// 	background: '#fff',
+// };
 
-const heading: CSSProperties = {
-	fontSize: 32,
-};
+// const heading: CSSProperties = {
+// 	fontSize: 32,
+// };
 
 export default WelcomeTemplate;
