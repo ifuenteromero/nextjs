8977dbd1052dad0770e2cca5543ee100c9f74cff
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-08-01 16:35:25 +0200

    Local fonts: poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter, Roboto } from 'next/font/google';
+import localFont from 'next/font/local';
 import AuthProvider from './auth/Provider';
 import './globals.css';
 import GoogleAnalyticsScript from './GoogleAnalyticsScript';
@@ -8,6 +9,10 @@ import NavBar from './NavBar';
 // variable fonts use a single file to represent a wide range of font styles, so we no need to specify the font weight
 const inter = Inter({ subsets: ['latin'] });
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '900'] });
+
+const poppins = localFont({
+	src: '../public/fonts/poppins-regular-webfont.woff2',
+});
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -22,7 +27,7 @@ export default function RootLayout({
 	return (
 		<html data-theme='winter' lang='en'>
 			<GoogleAnalyticsScript />
-			<body className={roboto.className}>
+			<body className={poppins.className}>
 				<AuthProvider>
 					<NavBar />
 					<main className='p-5'>{children}</main>
